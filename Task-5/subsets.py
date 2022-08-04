cca47dd54e8bc6539272eb23d6fc452ad0ece888
dhreeti414 <85941578+dhreeti414@users.noreply.github.com>
2022-08-04 23:07:43 +0530

    Delete Task-5 directory

--- Task-5/subsets.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        n=len(nums)
-        return [lst for lsts in [list(combinations(nums,i)) for i in range(n+1)] for lst in lsts]
